5b1238e006b97e4a055b8d4d39f6c0001f3704e8
David Elder <elddm1@gmail.com>
2016-06-19 13:32:13 -0400

    Set correct aniso filter mode for scene sampler.

--- Engine/Source/Scene.cpp
+++ Engine/Source/Scene.cpp
@@ -255,6 +255,7 @@ void Scene::Initialize()
 	auto samplerDesc = CD3D11_SAMPLER_DESC(D3D11_DEFAULT);
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
 	samplerDesc.MaxAnisotropy = 8;
 	
 	ThrowIfFailed(g_device->CreateSamplerState(&samplerDesc, m_samplerState.GetAddressOf()));
